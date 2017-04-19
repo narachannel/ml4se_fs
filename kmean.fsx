b73e24439b945900681892c4b857ee2ef7d718ff
Koji Narazaki <koji@narachannel.com>
2017-04-19 22:47:55 +0900

    kmean added

---
+++ kmean.fsx
@@ -0,0 +1,11 @@
+#I "paket-files/include-scripts/net46"
+#load "include.xplot.plotly.fsx"
+#load "packages/Deedle/Deedle.fsx"
+#load "include.mathnet.numerics.fsharp.fsx"
+
+open System
+open XPlot.Plotly
+open Deedle
+open MathNet.Numerics
+open MathNet.Numerics.LinearAlgebra
+open MathNet.Numerics.Distributions
