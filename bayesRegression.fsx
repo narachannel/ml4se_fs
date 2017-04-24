8b332df4cdd8b98ba213dc8fab70e3fad874e891
Koji Narazaki <koji@narachannel.com>
2017-04-24 22:32:06 +0900

    Dataset added

--- bayesRegression.fsx
+++ bayesRegression.fsx
@@ -12,13 +12,6 @@ open MathNet.Numerics
 open MathNet.Numerics.LinearAlgebra
 open MathNet.Numerics.Distributions
 
-(*
-    def create_dataset(num):
-    dataset = DataFrame(columns=['x','y'])
-    for i in range(num):
-        x = float(i)/float(num-1)
-        y = np.sin(2*np.pi*x) + normal(scale=0.3)
-        dataset = dataset.append(Series([x,y], index=['x','y']),
-                                 ignore_index=True)
-    return dataset
-*)
+let x = [0.0 .. 0.1 .. 1.0]
+let y = x |> List.map (fun ls -> sin (2.0 * Math.PI * ls) + Normal(0.0, 0.3).Sample())
+
