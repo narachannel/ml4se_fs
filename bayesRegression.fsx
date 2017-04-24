d3d48d4bfb5a0e4c47748fcf798691e2ccb8c307
Koji Narazaki <koji@narachannel.com>
2017-04-24 22:30:47 +0900

    Bayes Regression added

--- bayesRegression.fsx
+++ bayesRegression.fsx
@@ -12,11 +12,13 @@ open MathNet.Numerics
 open MathNet.Numerics.LinearAlgebra
 open MathNet.Numerics.Distributions
 
-
-let imageToArray = 
-    let image = Image.FromFile("photo.jpg")
-    let ms = new MemoryStream()
-    image.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg)
-    ms.ToArray() |> Array.map float
-
-imageToArray
+(*
+    def create_dataset(num):
+    dataset = DataFrame(columns=['x','y'])
+    for i in range(num):
+        x = float(i)/float(num-1)
+        y = np.sin(2*np.pi*x) + normal(scale=0.3)
+        dataset = dataset.append(Series([x,y], index=['x','y']),
+                                 ignore_index=True)
+    return dataset
+*)
